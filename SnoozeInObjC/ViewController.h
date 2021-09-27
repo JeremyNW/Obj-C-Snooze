9277a1bf6c030afc57c0275e611350f256f4a0e5
JeremyNW <jeddynwarren@gmail.com>
2021-09-27 17:50:01 -0600

    Initial Commit

--- SnoozeInObjC/ViewController.h
+++ SnoozeInObjC/ViewController.h
@@ -9,6 +9,13 @@
 
 @interface ViewController : UIViewController
 
+// MARK: - Data
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property (weak, nonatomic) NSTimer *timer;
+@property (nonatomic) NSInteger timeLeft;
+
+// MARK: - Behaviors
+- (IBAction)snoozeButtonTapped:(id)sender;
 
 @end
 
--- SnoozeInObjC/ViewController.m
+++ SnoozeInObjC/ViewController.m
@@ -15,8 +15,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
+
+- (IBAction)snoozeButtonTapped:(id)sender {
+    [self.timer invalidate];
+    self.timeLeft = 30;
+    self.timerLabel.text = @"30";
+    self.timerLabel.textColor = UIColor.systemGreenColor;
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(updateTimer) userInfo:NULL repeats:YES];
+}
+
+- (void)updateTimer {
+    self.timeLeft -= 1;
+    self.timerLabel.text = [NSString stringWithFormat:@"%ld", (long)self.timeLeft];
+
+    if (self.timeLeft <= 0) {
+        [self.timer invalidate];
+        self.timerLabel.textColor = UIColor.systemRedColor;
+    }
+}
 
 @end
